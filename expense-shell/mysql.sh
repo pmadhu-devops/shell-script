8111f39967d457f7f316d9ede92ad811c9d3deb3
pmadhu-devops <panchakatlamadu1@gmail.com>
2025-09-13 11:54:23 +0530

    mysql setup

--- expense-shell/mysql.sh
+++ expense-shell/mysql.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# variables setup
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+# color messages
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -11,6 +16,8 @@ N="\e[0m"
 echo "Please enter DB password:"
 read -s mysql_root_password
 
+
+# validate function
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
@@ -21,6 +28,8 @@ VALIDATE(){
     fi
 }
 
+# checking super/root user or not
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
@@ -30,12 +39,16 @@ else
 fi
 
 
+# installing mysql server
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Server"
+
+# enabling mysql server
 
 systemctl enable mysqld &>>$LOGFILE
 VALIDATE $? "Enabling MySQL Server"
 
+# Starting mysql server
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MySQL Server"
 
